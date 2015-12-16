e5e2cff0c8dea70cc402810e27ac656176ddaf83
Andrew Persad <akpersad@gmail.com>
2015-12-16 10:19:08 -0500

    done

--- lib/my_length.rb
+++ lib/my_length.rb
@@ -1,2 +1,13 @@
 # your code goes here
 # you will be adding the method #my_length to the Array class
+require 'pry'
+
+class Array
+	def my_length
+		arr = []
+		self.each_with_index do |item, index|
+			arr << index
+		end
+		arr[-1] + 1
+	end
+end
